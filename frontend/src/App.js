b9ab453f01412d60cce8f3536d8cc1e7525f3963
Enrique1311 <ejspinelli131176@gmail.com>
2022-10-10 20:48:56 -0300

    Primer Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+	return (
+		<div className='App'>
+			<header>
+				<a href='/'>Ecommerce</a>
+			</header>
+			<main>Lista de productos</main>
+		</div>
+	);
+}
+
+export default App;
