b53c4907b31a8adc607a0194407e0df9603d5682
Enrique1311 <ejspinelli131176@gmail.com>
2022-10-12 00:35:53 -0300

    Se agregó Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,28 @@
-import MyButton from './components/utils/MyButton/MyButton';
-import data from './data';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Products from './components/Products';
 
 function App() {
 	return (
-		<div className='App'>
-			<header>
-				<a href='/'>Ecommerce</a>
-			</header>
-			<main>
-				<h1>Productos Destacados</h1>
-				<div className='products'>
-					{data.products.map((product) => (
-						<div
-							className='product'
-							key={product.slug}
-						>
-							<a href={`/product/${product.slug}`}>
-								<img
-									src={product.image}
-									alt={product.name}
-								/>
-							</a>
-							<div className='product-description'>
-								<a
-									href={`/product/${product.slug}`}
-									className='product-name'
-								>
-									<p>{product.name}</p>
-								</a>
-								<p className='product-price'>$ {product.price}</p>
-								<MyButton>Agregar al carrito</MyButton>
-							</div>
-						</div>
-					))}
-				</div>
-			</main>
-		</div>
+		<BrowserRouter>
+			<div className='App'>
+				<header>
+					<Link to='/'>Ecommerce</Link>
+				</header>
+				<main>
+					<Routes>
+						<Route
+							path='/product/:slug'
+							element={<Products />}
+						/>
+						<Route
+							path='/'
+							element={<Home />}
+						/>
+					</Routes>
+				</main>
+			</div>
+		</BrowserRouter>
 	);
 }
 
